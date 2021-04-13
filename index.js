6bb58a020bb69db08f763bb14f9680947d5324ab
Rose <r.d.jeanlouis01@gmail.com>
2021-04-12 23:21:27 -0400

    commit tree 321d74a8822670ddee0ec8ac786809093fc7621d

---
+++ index.js
@@ -0,0 +1,11 @@
+//divide fuction 
+let divide = function (){
+   const divide = 2000/100; 
+   return divide;
+}
+
+//square function
+const square = (num) => num *num;
+
+//add function 
+const add = (x,y) => x+y;
